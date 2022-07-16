2cb0a90cbdfccf23add47c30ae2dd62ad66fcbad
ZHammerl <108287700+ZHammerl@users.noreply.github.com>
2022-07-16 16:07:58 +0200

    created an array with 3 cocktails

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,24 @@ alert('Hello world');
 
 let favouriteFood = 'Enchiladas verdes';
 document.write(favouriteFood);
+
+let cocktailList = [
+  {
+    name: 'Margarita',
+    tags: ['alcoholic', 'contemporary classic'],
+    glass: 'Cocktail glass',
+    ingredient1: 'Tequila',
+  },
+  {
+    name: 'Mojito',
+    tags: ['alcoholic', 'contemporary classic'],
+    glass: 'Highball glass',
+    ingredient1: 'White rum',
+  },
+  {
+    name: 'Dry Martini',
+    tags: ['alcoholic', 'classic'],
+    glass: 'Cocktail glass',
+    ingredient1: 'Gin',
+  },
+];
