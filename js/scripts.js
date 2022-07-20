1ce033a94807c5adf2e9f10e81c03a3958fe595f
ZHammerl <108287700+ZHammerl@users.noreply.github.com>
2022-07-20 16:52:45 +0200

    turned cocktailList Array into an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,41 @@
-let cocktailList = [
-  {
-    name: 'Margarita',
-    tags: ['alcoholic', 'contemporary classic'],
-    glass: 'Cocktail glass',
-    ingredient1: 'Tequila',
-  },
-  {
-    name: 'Mojito',
-    tags: ['alcoholic', 'contemporary classic'],
-    glass: 'Highball glass',
-    ingredient1: 'White rum',
-  },
-  {
-    name: 'Dry Martini',
-    tags: ['alcoholic', 'classic'],
-    glass: 'Cocktail glass',
-    ingredient1: 'Gin',
-  },
-];
+let cocktailRepository = (function () {
+  let cocktailList = [
+    {
+      name: 'Margarita',
+      tags: ['alcoholic', 'contemporary classic'],
+      glass: 'Cocktail glass',
+      ingredient1: 'Tequila',
+    },
+    {
+      name: 'Mojito',
+      tags: ['alcoholic', 'contemporary classic'],
+      glass: 'Highball glass',
+      ingredient1: 'White rum',
+    },
+    {
+      name: 'Dry Martini',
+      tags: ['alcoholic', 'classic'],
+      glass: 'Cocktail glass',
+      ingredient1: 'Gin',
+    },
+  ];
+
+  function add(item) {
+    cocktailList.push(item);
+  }
+
+  function getAll() {
+    return cocktailList;
+  }
+
+  return {
+    add,
+    getAll,
+  };
+})();
 
 // for loop iterates over cocktail list and writes their names and what type of glass they are served in
-cocktailList.forEach(function (cocktail) {
+cocktailRepository.getAll().forEach(function (cocktail) {
   //writes special text for cocktails served in a special glass
   if (cocktail.glass !== 'Cocktail glass') {
     document.write(
