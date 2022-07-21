eafe6bb7664c0a31f0b957aff20a4c2a31c797b4
ZHammerl <108287700+ZHammerl@users.noreply.github.com>
2022-07-21 11:39:54 +0200

    adding a filter by name option

--- js/scripts.js
+++ js/scripts.js
@@ -20,14 +20,21 @@ let cocktailRepository = (function () {
     },
   ];
 
+  // validation of type of added item and if all objectkeys are the same of the orginial array
   function add(item) {
     if (typeof item === 'object' && Object.keys(cocktailList[0]).every((key) => key in item)) {
       cocktailList.push(item);
-    }
+    } else {alert('Please make sure to fill in all necessary data.')}
   }
 
   function getAll() {
     return cocktailList;
+  }
+
+  // filter cocktails by name ?? why can I not use result in an if statement??
+  function filter(input) {
+    let result = cocktailList.filter((e) => e.name === input);
+    document.write(result[0].name + ' is served in a ' + result[0].glass);
   }
 
   return {
